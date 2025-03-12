02d2335e75a902400d0881e5f74fb3b0194838ea
Laura Segre <laura.segre@kellogg.northwestern.edu>
2025-03-12 15:19:45 +0000

    added user authentication

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,18 +1,21 @@
 class EntriesController < ApplicationController
-
   def new
   end
-
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = params["user_id"]
-    @entry.uploaded_image.attach(params["uploaded_image"])
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    if session["user_id"]
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = session["user_id"]
+      if @entry.save
+        redirect_to "/places/#{@entry["place_id"]}", notice: "Entry created successfully!"
+      else
+        redirect_to "/entries/new", notice: "Failed to create entry."
+      end
+    else
+      redirect_to "/login", notice: "You must be logged in to create an entry."
+    end
   end
-
 end
