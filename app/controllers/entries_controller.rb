b05082e664d708ff0261ac22c90a7cfc165c022e
Laura Segre <laura.segre@kellogg.northwestern.edu>
2025-03-12 15:33:32 +0000

    updated entries controller and index

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,21 +1,42 @@
 class EntriesController < ApplicationController
-  def new
+  before_action :require_login, only: [:index, :new, :create, :show]
+
+  def index
+    # Only show entries created by the logged-in user
+    @entries = Entry.where(user_id: session["user_id"])
   end
+
+  def new
+    @entry = Entry.new
+  end
+
   def create
-    if session["user_id"]
-      @entry = Entry.new
-      @entry["title"] = params["title"]
-      @entry["description"] = params["description"]
-      @entry["occurred_on"] = params["occurred_on"]
-      @entry["place_id"] = params["place_id"]
-      @entry["user_id"] = session["user_id"]
-      if @entry.save
-        redirect_to "/places/#{@entry["place_id"]}", notice: "Entry created successfully!"
-      else
-        redirect_to "/entries/new", notice: "Failed to create entry."
-      end
+    @entry = Entry.new(entry_params)
+    @entry.user_id = session["user_id"] # Assign entry to the logged-in user
+
+    if @entry.save
+      redirect_to "/places/#{@entry.place_id}", notice: "Entry created successfully!"
     else
-      redirect_to "/login", notice: "You must be logged in to create an entry."
+      redirect_to "/entries/new", alert: "Failed to create entry."
+    end
+  end
+
+  def show
+    @entry = Entry.find_by(id: params[:id], user_id: session["user_id"])
+    if @entry.nil?
+      redirect_to entries_path, alert: "You are not authorized to view this entry."
+    end
+  end
+
+  private
+
+  def entry_params
+    params.permit(:title, :description, :occurred_on, :place_id)
+  end
+
+  def require_login
+    unless session["user_id"]
+      redirect_to "/login", alert: "You must be logged in to access this section."
     end
   end
 end
