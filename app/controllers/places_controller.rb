3f1e922770bd640ea55b9d8650644b6973f45f9f
Laura Segre <laura.segre@kellogg.northwestern.edu>
2025-03-13 22:43:41 +0000

    changed places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,7 +7,6 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find_by({ "id" => params["id"] })
     @entries = Entry.where({"user_id" => current_user.id, "place_id" => @place["id"] })
-
   end
 
   def new
